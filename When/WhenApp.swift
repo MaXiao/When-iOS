a0ded11bfdcbe18c1bd9c5103b8382f33e163322
Xiao Ma <xma@figure1.com>
2023-01-21 15:47:07 -0500

    Initial Commit

---
+++ When/WhenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhenApp.swift
+//  When
+//
+//  Created by Xiao Ma on 2023-01-21.
+//
+
+import SwiftUI
+
+@main
+struct WhenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
